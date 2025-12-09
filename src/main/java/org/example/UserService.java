1daad6543131d259319643f5d86f5e60b4bfa6f7
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2025-12-09 13:43:05 +0300

    implement user registration validation logic

--- src/main/java/org/example/UserService.java
+++ src/main/java/org/example/UserService.java
@@ -1,7 +1,18 @@
 package org.example;
 
 public class UserService {
-    public static void main() {
-
-     }
+    public boolean registerUser(String userNameControl, String passwordControl, int ageControl){
+        if (userNameControl == null || userNameControl.length()<3) {
+            throw new IllegalArgumentException("Please enter a valid Username.(It should be at least 3 character long)");
+        }
+        if (passwordControl.toLowerCase().contains("admin") || passwordControl.length()<6){
+            throw new IllegalArgumentException("Your password must have at least 6 characters & not contain admin.");
+        }
+        if (ageControl<18 || ageControl>100){
+            throw new IllegalArgumentException("Unfortunately your age is not acceptable");
+        }
+        else{
+            return true;
+        }
+    }
  }
